96fc03c69fb8eaccea352522c5eb39a3eca5a438
mariosantosnn <mariosantosnn@gmail.com>
2022-11-09 15:28:52 +0000

    added server prototype

--- parser/Parser.java
+++ parser/Parser.java
@@ -2,6 +2,21 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
+//Takes several lines from the file and splits them by spaces
+
+//FILE:
+//UM DOIS
+//TRÊS QUATRO
+//CINCO SEIS
+
+//RETURNS:
+//UM
+//DOIS
+//TRÊS
+//QUATRO
+//CINCO
+//SEIS
+
 public class Parser {
 
 	public static void main(String[] args) {
