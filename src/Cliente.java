83d59cf434aaac95c1eba8c049846dc235d0b6ee
Casal Novo <52653136+DCasalNovo@users.noreply.github.com>
2022-11-09 20:00:04 +0000

    client

---
+++ src/Cliente.java
@@ -0,0 +1,13 @@
+public class Cliente {
+
+    public static void main(String[] args) {
+        if (args.length >= 3){
+            String serverIP = args[0];
+            String name = args[1];
+            String type = args[2];
+            boolean flagR = (args.length == 4);
+
+
+        }
+    }
+}
